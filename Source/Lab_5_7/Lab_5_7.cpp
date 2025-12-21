3fc0f054db2f0007eec69b0789cdef6b7ddb4e01
Ruben Montes <ruben.montes.mendoza@gmail.com>
2025-12-21 13:24:49 +0100

    first commit

---
+++ Source/Lab_5_7/Lab_5_7.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Lab_5_7.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Lab_5_7, "Lab_5_7" );
